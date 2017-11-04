a60e22add5b7c7f51af6857e882a08423d10f6f1
casey-rock <catnip.fatty@gmail.com>
2017-11-04 16:10:05 -0400

    Changed my mind - went back to the player1 and player2 movement scripts :) however, like was happening before, player2 is kind of along for the ride, rotating around a camera that's not attached to the body, and for some reason, firing the little ball isn't working (player2 should fire with spacebar, player 1 with left mouse click)

--- Assets/Scripts/player2.cs
+++ Assets/Scripts/player2.cs
@@ -2,6 +2,10 @@
 
 public class player2 : MonoBehaviour
 {
+	//create bullet
+	public GameObject bulletPrefab1;
+	public Transform bulletSpawn1;
+
     void Update()
     {
         var x = Input.GetAxis("Horizontal1") * Time.deltaTime * 150.0f;
@@ -9,5 +13,25 @@ public class player2 : MonoBehaviour
 
 		transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+		//Fire on space
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Fire();
+		}
     }
+
+	//Fire function
+	void Fire(){
+		//create bullet
+		var bullet1 = (GameObject)Instantiate (
+			bulletPrefab1,
+			bulletSpawn1.position,
+			bulletSpawn1.rotation);
+
+		// Add velocity to the bullet
+		bullet1.GetComponent<Rigidbody>().velocity = bullet1.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet1, 2.0f); 
+	}
 }
