b743c7e1bdfacb9a5368d43e004b09f30f4342c8
casey-rock <catnip.fatty@gmail.com>
2017-11-10 15:56:36 -0500

    disabled unnecessary audio listeners, got dropping working, last supper and the dream go to their own spaces on the wall

--- Assets/Scripts/dropMe.cs
+++ Assets/Scripts/dropMe.cs
@@ -8,32 +8,30 @@ using UnityEngine;
 
 public class dropMe : MonoBehaviour {
 
-
-		public GameObject item; //object we want to drop
+	public GameObject item; //object we want to drop
+	public GameObject tempParent;
 	public Transform drop; //where the object will go when dropped.
 
 	bool playerInTrigger; ///this is used to indicate player is in 'drop zone'
 
-//this detect if player 1 is in 'drop area' to drop object
+	//this detect if player 1 is in 'drop area' to drop object
 	void OnTriggerEnter (Collider col)
 	{
-		if(col.gameObject.name == "player1")
-	{
+		if(col.gameObject.tag == "Player")
+		{
 			Debug.Log("hit");
 
-		playerInTrigger = true;
-	
+			playerInTrigger = true;
+		}
 	}
-
-}
 
 //this says if player is in 'drop area' and they press the right key, they will pick up object
 	void Update(){
-		if(playerInTrigger && Input.GetKeyDown("return")){
-           item.GetComponent<Rigidbody>().useGravity = true;
-		item.GetComponent<Rigidbody>().isKinematic = false;
-		item.transform.parent = null;
-		item.transform.position = drop.transform.position;
-	}
+		if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftControl) && item.transform.IsChildOf(tempParent.transform)){
+			item.GetComponent<Rigidbody>().useGravity = false;
+			item.GetComponent<Rigidbody>().isKinematic = false;
+			item.transform.parent = null;
+			item.transform.position = drop.transform.position;
+		}
 	}
 }
