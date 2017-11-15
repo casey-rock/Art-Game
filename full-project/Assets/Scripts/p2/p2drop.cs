ca6f092a8be71464fc7feea819b9fb900eb148f8
casey-rock <33061933+casey-rock@users.noreply.github.com>
2017-11-15 11:37:58 -0500

    added 1p and 2p controller support

--- full-project/Assets/Scripts/p2/p2drop.cs
+++ full-project/Assets/Scripts/p2/p2drop.cs
@@ -68,91 +68,91 @@ void Start () {
 //this says if player is in 'drop area' and they press the right key, they will pick up object
 	void Update(){
 
-			if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item.transform.IsChildOf(tempParent.transform)){
+		if(playerInTrigger && Input.GetButtonDown("Submit") && item.transform.IsChildOf(tempParent.transform)){
 			item.GetComponent<Rigidbody>().useGravity = false;
 			item.GetComponent<Rigidbody>().isKinematic = false;
 			item.transform.parent = null;
 			item.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item1.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item1.transform.IsChildOf(tempParent.transform)){
 			item1.GetComponent<Rigidbody>().useGravity = false;
 			item1.GetComponent<Rigidbody>().isKinematic = false;
 			item1.transform.parent = null;
 			item1.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item2.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item2.transform.IsChildOf(tempParent.transform)){
 			item2.GetComponent<Rigidbody>().useGravity = false;
 			item2.GetComponent<Rigidbody>().isKinematic = false;
 			item2.transform.parent = null;
 			item2.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item3.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item3.transform.IsChildOf(tempParent.transform)){
 			item3.GetComponent<Rigidbody>().useGravity = false;
 			item3.GetComponent<Rigidbody>().isKinematic = false;
 			item3.transform.parent = null;
 			item3.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item4.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item4.transform.IsChildOf(tempParent.transform)){
 			item4.GetComponent<Rigidbody>().useGravity = false;
 			item4.GetComponent<Rigidbody>().isKinematic = false;
 			item4.transform.parent = null;
 			item4.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item5.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item5.transform.IsChildOf(tempParent.transform)){
 			item5.GetComponent<Rigidbody>().useGravity = false;
 			item5.GetComponent<Rigidbody>().isKinematic = false;
 			item5.transform.parent = null;
 			item5.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item6.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item6.transform.IsChildOf(tempParent.transform)){
 			item6.GetComponent<Rigidbody>().useGravity = false;
 			item6.GetComponent<Rigidbody>().isKinematic = false;
 			item6.transform.parent = null;
 			item6.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item7.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item7.transform.IsChildOf(tempParent.transform)){
 			item7.GetComponent<Rigidbody>().useGravity = false;
 			item7.GetComponent<Rigidbody>().isKinematic = false;
 			item7.transform.parent = null;
 			item7.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item8.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item8.transform.IsChildOf(tempParent.transform)){
 			item8.GetComponent<Rigidbody>().useGravity = false;
 			item8.GetComponent<Rigidbody>().isKinematic = false;
 			item8.transform.parent = null;
 			item8.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item9.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item9.transform.IsChildOf(tempParent.transform)){
 			item9.GetComponent<Rigidbody>().useGravity = false;
 			item9.GetComponent<Rigidbody>().isKinematic = false;
 			item9.transform.parent = null;
 			item9.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item10.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item10.transform.IsChildOf(tempParent.transform)){
 			item10.GetComponent<Rigidbody>().useGravity = false;
 			item10.GetComponent<Rigidbody>().isKinematic = false;
 			item10.transform.parent = null;
 			item10.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item11.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item11.transform.IsChildOf(tempParent.transform)){
 			item11.GetComponent<Rigidbody>().useGravity = false;
 			item11.GetComponent<Rigidbody>().isKinematic = false;
 			item11.transform.parent = null;
 			item11.transform.position = drop.transform.position;
 			gameController.AddScore (1);
 			}
-				else if(playerInTrigger && Input.GetKeyDown(KeyCode.LeftShift) && item12.transform.IsChildOf(tempParent.transform)){
+		else if(playerInTrigger && Input.GetButtonDown("Submit") && item12.transform.IsChildOf(tempParent.transform)){
 			item12.GetComponent<Rigidbody>().useGravity = false;
 			item12.GetComponent<Rigidbody>().isKinematic = false;
 			item12.transform.parent = null;
