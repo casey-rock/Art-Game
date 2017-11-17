92e09b417f9c995626d2f5be6a8e14bf68edf09d
casey-rock <catnip.fatty@gmail.com>
2017-11-16 21:23:09 -0500

    game design edits

--- full-project/Assets/Scripts/player1.cs
+++ full-project/Assets/Scripts/player1.cs
@@ -9,7 +9,7 @@ public class player1 : MonoBehaviour
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 15.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
